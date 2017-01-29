cf00e166b45706cc35301e2ec3b0712504a1a551
brugue_m <maxime.bruguet@epitech.eu>
2017-01-29 12:55:50 +0100

    Clean + string

--- app/src/main/java/xyz/maxime_brgt/todolist/SplashScreenActivity.java
+++ app/src/main/java/xyz/maxime_brgt/todolist/SplashScreenActivity.java
@@ -7,9 +7,6 @@ import android.os.Bundle;
 
 public class SplashScreenActivity extends Activity {
 
-    // Splash screen timer
-    private static int SPLASH_TIME_OUT = 3000;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,15 +21,11 @@ public class SplashScreenActivity extends Activity {
 
             @Override
             public void run() {
-                // This method will be executed once the timer is over
-                // Start your app main activity
                 Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
                 startActivity(i);
-
-                // close this activity
                 finish();
             }
-        }, SPLASH_TIME_OUT);
+        }, 3000);
     }
 
 }
